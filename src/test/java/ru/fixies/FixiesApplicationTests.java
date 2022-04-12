cd0224dcbf8357d3dc3ac81d0a23cdde7e718ac3
Evgeniy Sklyarov <skevni@gmail.com>
2022-04-12 21:18:54 +0300

    структура проекта

---
+++ src/test/java/ru/fixies/FixiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fixies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
