3860126eaa5c3f249453504ca58199e24dee83d5
Evgeniy Sklyarov <skevni@gmail.com>
2022-05-04 19:57:43 +0300

    added scripts

---
+++ src/main/resources/static/order_status/order_status.js
@@ -0,0 +1,27 @@
+angular.module('fx-front').controller('orderStatusController', function ($scope, $http, $routeParams, $location, $localStorage) {
+    const contextPath = 'http://localhost:8159/asc';
+
+
+    $scope.displayOrderStatus = function () {
+        $http({
+            url: contextPath + '/api/v1/orders' + '/status',
+            method: 'GET',
+            data: $scope.newOrder
+        }).then(function (response) {
+            alert('Ваш заказ успешно сформирован');
+            $scope.newOrder = null;
+            $location.path('/');
+        }, function failureCallback (response) {
+            console.log($scope.newOrder);
+            $scope.newOrder = null;
+            alert(response.data.messages);
+        });
+    };
+
+
+    $scope.getModelsForOrderPage();
+    $scope.getRepairStatusesForOrderPage();
+    $scope.getUsersForOrderPage();
+
+
+});
