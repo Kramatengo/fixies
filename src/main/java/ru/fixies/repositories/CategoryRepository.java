33932c8d80a8e3e80d92c8efcf72b47129cd7e96
AndrewMosk <MoskaletsAndrey@yandex.ru>
2022-05-10 11:49:58 +0300

    add category search by model id for order form

--- src/main/java/ru/fixies/repositories/CategoryRepository.java
+++ src/main/java/ru/fixies/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package ru.fixies.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.fixies.models.Category;
 
@@ -9,4 +10,11 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
+
+    @Query(
+            value = "SELECT * " +
+                    "FROM categories " +
+                    "WHERE id IN (SELECT category_id FROM models WHERE id = :modelId) ",
+            nativeQuery = true)
+    Optional<Category> findByModelId(Long modelId);
 }
